82e08f912492d15af0352bdfb7423d5b82e16134
Andy Zelenak <andyz@utexas.edu>
2016-09-22 17:02:54 -0500

    Added an example of transforming FT data. Changed the example to an object-oriented style.

--- src/netft_utils_cpp_test.cpp
+++ src/netft_utils_cpp_test.cpp
@@ -26,44 +26,72 @@ Author: Andy Zelenak
 #include "ros/console.h"
 #include "netft_utils_lean.h"
 
-// Author: Andy Zelenak, 2016
 // Test the C++ interface for the ATI force/torque sensor with a Netbox.
 
-// Callback for the ft sensor
-void netftCallback(const geometry_msgs::WrenchStamped& wrench);
+// Most of the action happens in this class.
+// The underscores denote members of the class.
+class netftExample
+{
+  public:
+
+    ros::NodeHandle n_;
+    double ftSleep_; // Controls the data acquisition rate
+    NetftUtilsLean* fti_;
+    geometry_msgs::WrenchStamped wrench_; // For data from the sensor
+
+    // Constructor
+    netftExample();
+
+    // Most of the action happens here
+    void getFTData();
+};
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "netft_utils_cpp_test");
-  ros::NodeHandle n;
   ros::AsyncSpinner* spinner;
   spinner = new ros::AsyncSpinner(3);
   spinner->start();
-  double ftSleep; // controls the data acquisition rate
-  
-  // Subscribe to the F/T topic
-  ros::Subscriber ftSub = n.subscribe("/netft/netft_data", 1, netftCallback);
-  
-  // Connect and bias the ft sensor
-  NetftUtilsLean* fti = new NetftUtilsLean(&n);
-  fti->setFTAddress("192.168.1.85");
-  // Adjust the data acquisition rate and set the World and Sensor frames, respectively
-  fti->initialize(1/ftSleep, "base_link", "ee_link");
-  
-  // Set max and threshold force/torque readings
-  // Vaules below the thresholds will register as zero
-  fti->setMax(80.0, 8.0, 60.0, 6.0);
-  std::future<bool> ftThread;
-  ftThread = std::async(std::launch::async, &NetftUtilsLean::run, fti);
-  fti->biasSensor(1);
+
+  // Create a new netftExample object.
+  // Most of the action happens here
+  netftExample *myNetftExample = new netftExample();
   
   ros::waitForShutdown();
  
   return 0;
 }
 
-// Callback for the ft sensor.
-void netftCallback(const geometry_msgs::WrenchStamped& wrench)
+// Constructor for the netftExample class
+netftExample::netftExample()
 {
-  ROS_INFO_STREAM("x force: " << wrench.wrench.force.x);
+  // Connect and bias the ft sensor
+  fti_ = new NetftUtilsLean(&n_);
+  fti_->setFTAddress("192.168.1.84");
+  // Adjust the data acquisition rate and set the World and Sensor frames, respectively
+  ftSleep_ = 0.05; // Will yield a 20 Hz data acquisition rate
+  fti_->initialize(1/ftSleep_, "right_ur5_base_link", "right_ur5_ee_link");
+  
+  // Set max and threshold force/torque readings
+  // Vaules below the thresholds will register as zero
+  fti_->setMax(80.0, 8.0, 60.0, 6.0);
+  std::future<bool> ftThread;
+  ftThread = std::async(std::launch::async, &NetftUtilsLean::run, fti_);
+  fti_->biasSensor(1);
+
+  getFTData();
+}
+
+void netftExample::getFTData()
+{
+  while (ros::ok())
+  {
+    fti_->getToolData(wrench_);
+    ROS_INFO_STREAM("X force in the tool frame: " << wrench_.wrench.force.x);
+
+    fti_->getWorldData(wrench_);
+    ROS_INFO_STREAM("X force in the world frame: " << wrench_.wrench.force.x);
+
+    sleep(ftSleep_);
+  }
 }
